f3be547d06929866fedaf441c763ffe2d35d45d1
KASHI220 <kashi220kp@gmail.com>
2026-03-27 21:11:46 +0530

    Dsa practice

---
+++ DSA_practice/dsa/LongestPrefix.java
@@ -0,0 +1,26 @@
+package DSA_practice.dsa;
+
+public class LongestPrefix {
+    public static void main(String[] args) {
+
+
+        String[] str = {"apple", "app", "apply", "approve"};
+        LongestPrefix prefix = new LongestPrefix();
+        String result = prefix.longPrefix(str);
+        System.out.println(result);
+    }
+
+    public String longPrefix(String[] str) {
+        for (int i = 0; i < str[0].length(); i++) {
+            char c = str[0].charAt(i);
+
+            for (int j = 1; j < str.length; j++) {
+                if (i == str[j].length() || str[j].charAt(i) != c) {
+                    return str[0].substring(0, i);
+                }
+            }
+
+        }
+        return str[0];
+    }
+}
---
+++ DSA_practice/dsa/UnionOfTwoArray.java
@@ -0,0 +1,19 @@
+package DSA_practice.dsa;
+
+public class UnionOfTwoArray {
+    public static void main(String[] args){
+        int[] a = {1,3,5,7,9};
+        int[] b = {1,2,3,4,5,6,7,8,9};
+        int l;
+        if(a.length>=b.length){
+             l=a.length;
+        }
+        else{
+            l=b.length;
+        }
+        for(int i=0;i<l;i++){
+            
+        }
+
+    }
+}
